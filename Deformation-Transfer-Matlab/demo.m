6800d0f0f25c041f830d19e2ee27969c82f02516
confSMIPaper4 <confSMIPaper4>
2024-05-21 11:16:54 +0800

    add

--- Deformation-Transfer-Matlab/demo.m
+++ Deformation-Transfer-Matlab/demo.m
@@ -5,8 +5,8 @@ addpath('./1. BasicModules');
 % addpath('./2. Non_rigid_registration');
 %% Example
 if ~exist('VS','var') 
-    [VS, FS, NS] = read_obj_file('E:/data/xie661.obj');
-    [VS2, FS2, NS2] = read_obj_file('E:/data/xie771_4.obj');
+    [VS, FS, NS] = read_obj_file('E:/data/model661.obj');
+    [VS2, FS2, NS2] = read_obj_file('E:/data/model771_4.obj');
     [VT, FT, NT] = read_obj_file('E:/data/nt1.obj');    
 end
 corres = cell(size(FT,1),1);
